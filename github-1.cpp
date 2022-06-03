11895f1f90aa699ca7b66a0b1efaf41e7584cf17
jamilahmedjr <93552163+jamilahmedjr@users.noreply.github.com>
2022-06-03 15:43:37 -0400

    Add files via upload

---
+++ github-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main()
+{
+    double total = 0;
+
+
+    cout << "****************************************************************************" << endl;
+    cout << "**************** Welcome To my Github Page *********************************" << endl;
+    cout << "****************************************************************************" << endl;
+    cout << endl;
+    cout << endl;
+    float cost = 4.12;
+    float profit = 1.45;
+    float kg = 2.76;
+    cout << "Please enter the total number of kilograms of cheese produced: ";
+    cin >> total;
+    float hold = round(total / kg);
+
+
+
+    cout << "The number of containers to hold the cheese is:" << setw(30) << hold << endl;
+    cout << "The cost of producing " << hold << " container(s) of cheese is: $" << setw(23) << hold * cost << endl;
+    cout << "The profit from producing " << hold << " container(s) of cheese is: $" << setw(19) << hold * profit << endl;
+
+
+ 
+    return 0;
+}
